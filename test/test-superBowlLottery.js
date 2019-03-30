b584c8ba7baf1b68a1585b2f4c7e2c697f528d1b
Jason McLaughlin <jasonstephenm@gmail.com>
2019-03-29 22:03:21 -0500

    Write tests for ticketToBinary

--- test/test-superBowlLottery.js
+++ test/test-superBowlLottery.js
@@ -12,5 +12,25 @@ describe('Super Bowl Lottery', function() {
       expect(result).to.equal(0);
       done();
     });
+    it("converts a string with a single '0' to 512", function() {
+      let result = sBL.ticketToBinary('0');
+      expect(result).to.equal(512);
+    });
+    it("converts a string with a single '9' to 1", function() {
+      let result = sBL.ticketToBinary('9');
+      expect(result).to.equal(1);
+    });
+    it("converts a string with '6789' to 15", function() {
+      let result = sBL.ticketToBinary('6789');
+      expect(result).to.equal(15);
+    });
+    it('converts a string with 0-9 to 1023', function() {
+      let result = sBL.ticketToBinary('0123456789');
+      expect(result).to.equal(1023);
+    });
+    it("converts a string with multiple 9's to 1", function() {
+      let result = sBL.ticketToBinary('99999999');
+      expect(result).to.equal(1);
+    });
   });
 });
